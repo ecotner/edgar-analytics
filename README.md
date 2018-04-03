e1d8b341af061876e2c7104d11867c9cf1706abf
ecotner <2.71828cotner@gmail.com>
2018-04-03 09:38:51 -0700

    Updated python script

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# edgar-analytics
-Insight data engineering coding challenge
+# edgar-analytics: Insight data engineering coding challenge
+
+### Outline
+This program is a solution to the Insight coding challenge found
+[here](https://github.com/InsightDataScience/edgar-analytics/blob/master/README.md#output-file).
+The main objective is to summarize access to financial documents from a number of different users.
+The program consists of a single Python script (sessionization.py), an input CSV (log.csv) and text document
+(inactivity_period.txt), and an output text document (sessionization.txt).
+The Python script contains a Session class, which contains all relevant information about each session (IP address,
+time of first access, last time of access, number of documents accessed), and some methods for operating on this
+class, including time conversions (times are stored as seconds elapsed since the Unix epoch).
+The script points to the log.csv file, and iterates through each line, which corresponds to a single document request.
+The IP address of the user and the time of access is extracted, then the IP is checked against a dictionary to see
+if there are any existing sessions that need updating.
+If there is no existing entry in the dictionary, a new one is made.
+The program then checks to see if any of the existing sessions in the dictionary has exceeded the inactivity period,
+and if so, appends the session to a list (deleting it from the dictionary), which is then sorted by the time of the
+first request.
+It then iterates through the list, writing a summary of each session to file.
+If the end of the file is reached and some sessions still have not terminated, the same sorting/writing procedure is
+performed, just without the check for inactivity.
+Since there are multiple document requests per second, and time is effectively quantized at one second intervals,
+the program only checks for inactivity whenever the time changes, which speeds up execution by roughly a factor of the
+frequency of document requests.
+
+### Some observations:
+After looking at some real-world data from the SEC's website, I've noticed a couple things:
+* Most users request a small number of documents per session (mostly just a single document)
+    * But there is a decent fraction of users who request many tens or hundreds of documents per session
+* The rate of document requests is fairly high (between 10-20/sec)
+* The active session time is positively correlated with number of documents requested
+
+### Downloading/running the program
+Clone the repository with `git clone https://github.com/ecotner/edgar-analytics`, then run the script with ...
--- src/sessionization.py
+++ src/sessionization.py
@@ -17,15 +17,108 @@ session is updated though, so this may complicate things).***
 
 """
 
+# Module imports
+import csv
+import time
+from pathlib import Path
+
+# Define input CSV and inactivity_period paths
+INPUT_PATH = Path("../input/log.csv")
+OUTPUT_PATH = Path("../output/sessionization.txt")
+INACTIVITY_PATH = Path("../input/inactivity_period.txt")
+
+# Define Session class
 class Session(object):
+    """
+    Session object for maintaining information about individual sessions. Has methods for creating and updating each
+    session.
+    """
     def __init__(self, ip_address, start_time, doc_count):
         self.ip_address = ip_address
         self.start_time = start_time
+        self.most_recent_time = start_time
         self.doc_count = doc_count
 
     def increment_doc_count(self, n=1):
         self.doc_count += n
 
-    def session_len(self, current_time):
-        dt = current_time - self.start_time
-        return dt
+    def update_time(self, t):
+        self.most_recent_time = t
+
+    def summary_str(self):
+        out_str = ",".join([sess.ip_address, # IP address
+                            int_to_time(sess.start_time), # Date/time of first request
+                            int_to_time(sess.most_recent_time), # Date/time of last request
+                            str(int(sess.most_recent_time - sess.start_time + 1)), # Duration in sec
+                            str(sess.doc_count)]) # Document count
+        return out_str + "\n"
+
+# Define utility functions
+def time_to_int(date, time_):
+    """ Converts the date and time from the the log file into an easy-to-handle integer. """
+    date = [int(e) for e in date.split("-")] # Elements of this list are year, month, day
+    time_ = [int(e) for e in time_.split(":")] # Elements of this list are hour, minute, second
+    t = (date[0], date[1], date[2], time_[0], time_[1], time_[2], 0, 0, -1)
+    return time.mktime(t)
+
+def int_to_time(t):
+    """ Converts an integer representing the time since epoch to the format yyyy-mm-dd hh:mm:ss """
+    t_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(t))
+    return t_str
+
+
+session_dict = {} # Dictionary of Session objects, indexed by IP address
+
+# Open inactivity_period.txt and extract the inactivity period
+with open(INACTIVITY_PATH, "r") as fo:
+    inactivity_period = float(fo.read())
+# Clear the output file
+with open(OUTPUT_PATH, "w+") as fo:
+    fo.write("")
+
+# Open input file, initialize CSV reader
+with open(INPUT_PATH, "r") as fo:
+    reader = csv.DictReader(fo)
+    # Start scanning through each line in the CSV
+    i = 1
+    previous_time = 0
+    for row in reader:
+        print("Row {}".format(i))
+        i += 1
+        # The entries of each row are: ip, date, time, zone, cik, accession, extention, code, size, idx, norefer,
+        # noagent, find, crawler, and browser
+        # Read the line, extract the IP address and current time
+        ip = row['ip']
+        current_time = time_to_int(row['date'], row['time'])
+        # If the IP matches one in the session dictionary, update the Session
+        if ip in session_dict:
+            session_dict[ip].increment_doc_count()
+            session_dict[ip].update_time(current_time)
+        # Otherwise create a new Session and add it to the dictionary
+        else:
+            session_dict[ip] = Session(ip, current_time, 1)
+
+        # Iterate through all Sessions in session_dict to check for lapsed sessions
+        if (current_time != previous_time):
+            previous_time = current_time
+            output_list = []
+            for key in list(session_dict.keys()):
+                sess = session_dict[key]
+                # If Session has exceeded the inactivity_period
+                if current_time - sess.most_recent_time > inactivity_period:
+                    # Append entry to output list
+                    output_list.append((sess.start_time, sess.summary_str()))
+                    # Delete Session from session_dict
+                    del session_dict[key]
+            # Sort output list by start time, then write to file
+            for _, e in sorted(output_list, key=lambda x: x[0]):
+                with open(OUTPUT_PATH, "a+") as fo_out:
+                    fo_out.write(e)
+    # At end of input file, pretend all sessions have terminated
+    output_list = []
+    for key in session_dict:
+        sess = session_dict[key]
+        output_list.append((sess.start_time, sess.summary_str()))
+    for _, e in sorted(output_list, key=lambda x: x[0]):
+        with open(OUTPUT_PATH, "a+") as fo_out:
+            fo_out.write(e)
